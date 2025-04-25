cf392d08133df2e4176576a563bd9a6c2046cb6d
yunsy1103 <yunsy1103@naver.com>
2025-04-25 14:05:13 +0900

    fix: 홈 로고,디자인 수정

--- src/components/signup/SignupComplete.tsx
+++ src/components/signup/SignupComplete.tsx
@@ -9,14 +9,26 @@ interface SignupCompleteProps {
 export default function SignupComplete({ onNext }: SignupCompleteProps) {
   return (
     <div className="h-full flex flex-col">
-        <div className="text-center mb-10 mt-20">
-          <h1 className="text-2xl font-bold text-[#0a5ca8] mb-2">Crepe에</h1>
-          <p className="text-xl">오신 것을 환영합니다.</p>
+      <div className="flex-1 flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center space-y-0.5">
+          {/* 로고 + '에' 한 줄로 정렬 */}
+          <div className="flex items-center gap-0 ">
+            <img
+              src="/crepe-newlogosmall.png"
+              alt="Crepe Logo"
+              className="w-[120px] h-[100px] object-contain"
+            />
+            <span className="text-xl font-bold text-slate-700 leading-none pb-[1px]">에</span>
+          </div>
+
+          {/* 아래 텍스트 */}
+          <p className="text-xl font-bold text-slate-700 leading-none leading-tight">    오신 것을 환영합니다.</p>
         </div>
+      </div>
 
         <div className="flex-1 flex items-center justify-center">
           <div className="relative w-45 h-40">
-            <img src="/crepe_newlogo.png" alt="Crepe Logo" className="w-full h-full" />
+            <img src="/crepe-newlogo.png" alt="Crepe Logo" className="w-full h-full" />
           </div>
         </div>
       <div className="p-5">
