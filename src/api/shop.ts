7e5fe1c513f40d4358d724418393dea7716a1b28
bonun00 <lifesciences00@naver.com>
2025-05-10 17:33:05 +0900

    fix:토큰 하드코딩 수정

--- src/api/shop.ts
+++ src/api/shop.ts
@@ -1,6 +1,7 @@
 import { Store, StoreDetail } from "@/types/store";
 
 const API_BASE_URL = import.meta.env.VITE_API_SERVER_URL || "http://localhost:8080"
+const token = localStorage.getItem("accessToken");
 
 // 열려있는 가게 목록 조회
 export async function getStoreList(): Promise<Store[]> {
@@ -8,7 +9,7 @@ export async function getStoreList(): Promise<Store[]> {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `Bearer eyJhbGciOiJIUzM4NCJ9.eyJyb2xlIjoiVVNFUiIsInVzZXJUeXBlIjoiVVNFUiIsImVtYWlsIjoidXNlckBjcmVwZS5jby5rciIsInN1YiI6InVzZXJAY3JlcGUuY28ua3IiLCJpYXQiOjE3NDYyNTY1NzUsImV4cCI6MTc0NjI2MDE3NX0.3aPjQftJ8eV-eComQaJhA0YhVF9vdPapL7L3eZYCqpu8Z8j949LwR5wtCi3JsuWt`
+      Authorization: `Bearer ${token}`,
     },
   });
 
@@ -24,7 +25,7 @@ export async function getStoreDetail(storeId: number | string): Promise<StoreDet
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `Bearer eyJhbGciOiJIUzM4NCJ9.eyJyb2xlIjoiVVNFUiIsInVzZXJUeXBlIjoiVVNFUiIsImVtYWlsIjoidXNlckBjcmVwZS5jby5rciIsInN1YiI6InVzZXJAY3JlcGUuY28ua3IiLCJpYXQiOjE3NDYyNTY1NzUsImV4cCI6MTc0NjI2MDE3NX0.3aPjQftJ8eV-eComQaJhA0YhVF9vdPapL7L3eZYCqpu8Z8j949LwR5wtCi3JsuWt`
+      Authorization: `Bearer ${token}`,
     },
   });
 
